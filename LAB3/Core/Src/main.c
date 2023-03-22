1bd547889395fda600fd6f1d4b016211cdbcea45
napassorn29 <119843578+napassorn29@users.noreply.github.com>
2023-03-22 20:15:37 +0700

    Final

--- LAB3/Core/Src/main.c
+++ LAB3/Core/Src/main.c
@@ -22,6 +22,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
+//นภัสสร เตชะสมบูรณากิจ 64340500035
 #define IC_BUFFER_SIZE 20
 
 /* USER CODE END Includes */
